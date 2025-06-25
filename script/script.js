b95cbfcd58df07ee9d3828e66d099c2f8a939666
nano782 <nandinitandonsre@gmail.com>
2025-06-25 22:46:34 +0530

    updated the UI by integrating about section in main page

--- script/script.js
+++ script/script.js
@@ -60,3 +60,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const target = document.querySelector(this.getAttribute('href'));
       if (target) {
         target.scrollIntoView({ behavior: 'smooth' })};
+        
+    document.querySelectorAll('.nav-menu a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
